74879a459cef7a7da96a9e90d6b3854636b579fd
Travis Dixon <travis@growdata.com.au>
2020-06-07 20:43:17 +1000

    multiple ships

---
+++ README.md
@@ -0,0 +1,6 @@
+# boats example
+
+This is the source code for the web example of https://github.com/thetrav/boats
+
+Mostly I'm just experimenting with github pages.  
+I expect android/iOS to be the primary platforms for this thing
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {
 "assets/LICENSE": "3034ddf27fef3bfc05bf0d1010122e0c",
 "index.html": "9464571ed319b35d55d3552df0a2a59e",
 "/": "9464571ed319b35d55d3552df0a2a59e",
-"main.dart.js": "e701db22a7d9bb76fe770db40289f5c6"
+"main.dart.js": "1d7f6854999633716bce1398c1bc6c96"
 };
 
 self.addEventListener('activate', function (event) {
